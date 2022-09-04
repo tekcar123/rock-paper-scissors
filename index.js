db254ad93c2ace5499f2211a5943fe7a96912c5f
tekcar123 <derekool@icloud.com>
2022-09-03 22:14:23 -0400

    Add round function

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ function getComputerChoice() {
     }
 }
 
-// function get player choice (case insensitive)
-
 function getPlayerChoice() {
     let choice = prompt("What do you want to play?").toLowerCase()
     if (choice === "rock" || choice === "paper" || choice === "scissors") {
@@ -26,9 +24,57 @@ function getPlayerChoice() {
     }
 }
 
-console.log(getPlayerChoice())
-
 // function to play a round with computer and player choice
+
+function round(player, computer) {
+    if (player === "rock") {
+        if (computer === "rock") {
+            console.log("Tie.")
+            return "tie"
+        } else if (computer === "paper") {
+            console.log("The computer wins with paper!")
+            return "computer"
+        } else if (computer === "scissors") {
+            console.log("The player wins with rock!")
+            return "player"
+        } else {
+            console.error("wrong computer choice")
+            return "error"
+        }
+    } else if (player === "paper") {
+        if (computer === "rock") {
+                console.log("The player wins with paper !")
+                return "player"
+            } else if (computer === "paper") {
+                console.log("Tie.")
+                return "tie"
+            } else if (computer === "scissors") {
+                console.log("The computer wins with scissors !")
+                return "computer"
+            } else {
+                console.error("wrong computer choice")
+                return "error"
+            }
+    } else if (player === "scissors") {
+        if (computer === "rock") {
+                console.log("The computer wins with rock !")
+                return "computer"
+            } else if (computer === "paper") {
+                console.log("The player wins with scissors !")
+                return "player"
+            } else if (computer === "scissors") {
+                console.log("Tie.")
+                return "tie"
+            } else {
+                console.error("wrong computer choice")
+                return "error"
+            }
+    } else {
+        console.error("wrong player choice")
+        return "error"
+    }
+}
+
 
 // game function that asks how many rounds to play
 

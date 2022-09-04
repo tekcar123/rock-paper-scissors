b37e6cf46df1d641d2fdebaedc9d01a6af6abb64
tekcar123 <derekool@icloud.com>
2022-09-03 20:08:02 -0400

    Write getComputerChoice function

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 
 // function get computer choice
 
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()) * 3
+    if (choice === 1) {
+        return "rock"
+    } else if (choice === 2) {
+        return "paper"
+    } else if (choice === 3) {
+        return "scissors"
+    } else {
+        console.warn("incorrect random number")
+    }
+}
+
 // function get player choice (case insensitive)
 
 // function to play a round with computer and player choice

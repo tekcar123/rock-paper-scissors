52a027ec3a07a48a3c97413ee2b63137cb17ff30
tekcar123 <derekool@icloud.com>
2022-09-03 20:04:37 -0400

    pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+// function get computer choice
+
+// function get player choice (case insensitive)
+
+// function to play a round with computer and player choice
+
+// game function that asks how many rounds to play
+
+// console.log winner

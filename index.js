e922f0e596312c956d552ec0346dc4f2867c5826
tekcar123 <derekool@icloud.com>
2022-09-03 21:53:32 -0400

    Add getPlayerChoice function

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 'use strict'
 
-// function get computer choice
-
 function getComputerChoice() {
-    let choice = Math.floor(Math.random()) * 3
+    let choice = Math.floor(Math.random() * 3) + 1;
     if (choice === 1) {
-        return "rock"
+        return "rock";
     } else if (choice === 2) {
-        return "paper"
+        return "paper";
     } else if (choice === 3) {
-        return "scissors"
+        return "scissors";
     } else {
-        console.warn("incorrect random number")
+        console.warn("incorrect random number");
+        return choice;
     }
 }
 
 // function get player choice (case insensitive)
+
+function getPlayerChoice() {
+    let choice = prompt("What do you want to play?").toLowerCase()
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice;
+    } else {
+        console.warn("invalid input, try again.")
+        return getPlayerChoice();
+    }
+}
+
+console.log(getPlayerChoice())
 
 // function to play a round with computer and player choice
 

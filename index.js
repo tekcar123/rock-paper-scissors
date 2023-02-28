2c0e24b362730e0b1e670bc139252f65d319e2d7
tekcar123 <derekool@icloud.com>
2023-02-28 16:26:20 -0500

    finished

--- index.js
+++ index.js
@@ -1,102 +1,24 @@
-'use strict'
-
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3) + 1;
-    if (choice === 1) {
-        return "rock";
-    } else if (choice === 2) {
-        return "paper";
-    } else if (choice === 3) {
-        return "scissors";
-    } else {
-        console.warn("incorrect random number");
-        return choice;
-    }
+let a = document.getElementsByClassName("choice")
+for (b of a) {
+    b.addEventListener('click', myFunction)
 }
 
-function getPlayerChoice() {
-    let choice = prompt("What do you want to play?").toLowerCase()
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-        return choice;
-    } else {
-        console.warn("invalid input, try again.")
-        return getPlayerChoice();
-    }
-}
+let playerScore = 0
+let computerScore = 0
+let winnerMessage = document.getElementById("winnerMessage")
 
-function round(player, computer) {
-    if (player === "rock") {
-        if (computer === "rock") {
-            console.log("Tie.")
-            return "tie"
-        } else if (computer === "paper") {
-            console.log("The computer wins with paper!")
-            return "computer"
-        } else if (computer === "scissors") {
-            console.log("The player wins with rock!")
-            return "player"
-        } else {
-            console.error("wrong computer choice")
-            return "error"
-        }
-    } else if (player === "paper") {
-        if (computer === "rock") {
-                console.log("The player wins with paper !")
-                return "player"
-            } else if (computer === "paper") {
-                console.log("Tie.")
-                return "tie"
-            } else if (computer === "scissors") {
-                console.log("The computer wins with scissors !")
-                return "computer"
-            } else {
-                console.error("wrong computer choice")
-                return "error"
-            }
-    } else if (player === "scissors") {
-        if (computer === "rock") {
-                console.log("The computer wins with rock !")
-                return "computer"
-            } else if (computer === "paper") {
-                console.log("The player wins with scissors !")
-                return "player"
-            } else if (computer === "scissors") {
-                console.log("Tie.")
-                return "tie"
-            } else {
-                console.error("wrong computer choice")
-                return "error"
-            }
-    } else {
-        console.error("wrong player choice")
-        return "error"
-    }
-}
-
-function rounds() {
-    let input = prompt("How many points to win?")
-    return parseInt(input)
-}
-
-function game() {
-    let pointsToWin = rounds()
-    let playerScore = 0;
-    let computerScore = 0;
-    let result;
-    while (playerScore < pointsToWin && computerScore < pointsToWin) {
-        result = round(getPlayerChoice(), getComputerChoice());
-        if (result === "player") {
-            playerScore++;
-        } else if (result === "computer") {
-            computerScore++;
-        }
-        console.log(`player has ${playerScore} points, computer has ${computerScore} points. First to ${pointsToWin} wins`)
-    }
-    if (playerScore > computerScore) {
-        console.log("player wins !")
+function myFunction() {
+    let random = Math.random() * 3
+    if (random <= 1) {
+        playerScore += 1
+        document.getElementById("player").innerHTML = playerScore
+        document.getElementById("winnerMessage").innerHTML = "You won!"
+    } else if (random <= 2) {
+        computerScore += 1
+        document.getElementById("computer").innerHTML = computerScore
+        document.getElementById("winnerMessage").innerHTML = "You lost :("
     }
     else {
-        console.log("computer wins!")
+        document.getElementById("winnerMessage").innerHTML = "Tie."
     }
 }
-game()
